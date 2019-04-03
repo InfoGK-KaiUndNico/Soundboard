cafa93c613e99db19a54cb0d8f00c7e7d2c18618
maqnico <nicolasmq@t-online.de>
2019-04-03 15:52:13 +0200

    Added PressedKey Object

--- src/soundboard/Keyhandler.java
+++ src/soundboard/Keyhandler.java
@@ -4,12 +4,12 @@ import java.awt.event.*;
 import soundboard.Song;
 
 public class Keyhandler implements KeyListener{
+    public String PressedKey = "none";
 
     private void playkeyQ() {                        //Eigentlicher Befehl, der den Sound mit dem Key verbindet und dann abspielt
   Song soundQ = null;
         soundQ.Abspielen("soundboard/lib/Epic-Sax.wav");
-
-
+    PressedKey = "Q";
     System.out.println("Hallo Welt!");
     }
 
@@ -31,7 +31,7 @@ public class Keyhandler implements KeyListener{
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        PressedKey = "none";
     }
 
 }
