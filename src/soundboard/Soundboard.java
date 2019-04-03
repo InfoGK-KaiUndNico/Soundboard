1c48535b1feb27ad1cfc3cacfc773943c7d71bb3
kaiindra <39216440+kaiindra@users.noreply.github.com>
2019-04-03 13:39:35 +0200

    Deleted comments!

--- src/soundboard/Soundboard.java
+++ src/soundboard/Soundboard.java
@@ -1,22 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package soundboard;
 
 import soundboard.*;
 
-
-/**
- *
- * @author nicolas.masello
- */
 public class Soundboard {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
